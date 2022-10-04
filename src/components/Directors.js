ef97f406faa672f1c9f88b38b17d61b2fff8dab3
Echesa Imaculate Zepwa <imaculate.zepwa@student.moringaschool.com>
2022-10-04 10:43:41 +0300

    complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { directors } from "../data";
 
-/**This component should render the text Directors Page in an <h1>,
- *  and make a new <div> for each director. The <div> should
- *  contain the director's name and a <ul> with a list of their movies. */
 function Directors() {
 const displayDirector = directors.map((director) => (
   <div key={director.name}>
